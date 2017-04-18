ce5745a6e45aad7c93bcfc3d114206587a18585b
Vasiliy <kaktusv6@gmail.com>
2017-04-18 22:21:09 +1100

    В Model_News дописал метод create_news.

--- application/models/model_news.php
+++ application/models/model_news.php
@@ -1,12 +1,15 @@
 <?php
 class Model_News extends Model {
 
-    function get_new($id) {
+    function get_news($id) {
         $query = "CALL getNew($id)";
         return $this->pdo->query($query)->fetch(PDO::FETCH_OBJ);
     }
     
-    function create_news($title, $text) {
+    function create_news($title, $text, $urlImg, $countChars = 100) {
         $data = date("Y-m-d");
+        $query = "CALL addNews('$title', '$data', '$text', '$urlImg', $countChars)";
+        $this->pdo->query($query);
+        $_SESSION['successfulText'] = 'Successful add news';
     }
 }
